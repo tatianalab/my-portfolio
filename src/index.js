739c8d8771e1a4ff3bb99e94357423405f9353f0
Tatiana Labrousse <mvtatianalabrousse@gmail.com>
2021-01-23 20:40:29 -0500

    indiv project

--- src/index.js
+++ src/index.js
@@ -1,1 +1,25 @@
-console.log("Hello from src/index.js!");
+const open_btn = document.querySelector('.open-btn')
+const close_btn = document.querySelector('.close-btn')
+const nav = document.querySelectorAll('.nav')
+const projects = document.querySelectorAll('.project-tile')
+// const icon_tools = document.querySelectorAll('.icon-tools')
+
+open_btn.addEventListener('click', () => {
+  nav.forEach(nav_el => nav_el.classList.add('visible'))
+})
+
+close_btn.addEventListener('click', () => {
+  nav.forEach(nav_el => nav_el.classList.remove('visible'))
+})
+
+projects.forEach(project => {
+  project.addEventListener('mouseover', () =>{
+    project.classList.add('slide')
+  })
+})
+
+projects.forEach(project => {
+  project.addEventListener('mouseout', () =>{
+    project.classList.remove('slide')
+  })
+})
