598cad066953b9b8fdad69a9a0674dbe96acad63
Tatiana Labrousse <mvtatianalabrousse@gmail.com>
2021-01-26 11:20:40 -0500

    removed some js from project pages

--- src/components/toggle.js
+++ src/components/toggle.js
@@ -1,7 +1,14 @@
 const toggles = document.querySelectorAll('.faq-toggle');
+const togtitles = document.querySelectorAll('.faq-title');
 
 
 toggles.forEach(toggle => {
+  toggle.addEventListener('click', () => {
+    toggle.parentNode.classList.toggle('active');
+  });
+});
+
+togtitles.forEach(toggle => {
   toggle.addEventListener('click', () => {
     toggle.parentNode.classList.toggle('active');
   });
